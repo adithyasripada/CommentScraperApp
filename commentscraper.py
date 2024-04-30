129742f2ffc6832f82b3b9f4152adc0e97024841
Adithya Sripada <55552063+adithyasripada@users.noreply.github.com>
2024-04-30 12:59:33 +0100

    Update commentscraper.py

--- commentscraper.py
+++ commentscraper.py
@@ -3,9 +3,9 @@ import pandas as pd
 import csv
 import streamlit as st
 
-# st.title("""Instragram Comment Scraper""")
-st.set_page_config(page_title="OGT Web Toolbox",
-                   page_icon="🧬",
+st.title("""Instragram Comment Scraper""")
+st.set_page_config(page_title="Instragram Comment Scraper",
+                   page_icon="🦎",
                    layout="wide",
                    initial_sidebar_state="expanded")
 file = st.file_uploader("Upload a CSV file")
