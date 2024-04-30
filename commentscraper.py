a0675e810581e2b94c2f8f0a1bed041511396a17
Adithya Sripada <55552063+adithyasripada@users.noreply.github.com>
2024-04-30 13:23:40 +0100

    Update commentscraper.py

--- commentscraper.py
+++ commentscraper.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Instragram Comment Scraper",
                    initial_sidebar_state="expanded")
 
 st.title("""Instragram Comment Scraper""")
-file = st.file_uploader("Upload a CSV file")
+file = st.sidebar.file_uploader("Upload a CSV file")
 
 
 rapid_api_key = "524970b541msh85f0a580cda5de9p129a7ajsnc109631e2d31"
